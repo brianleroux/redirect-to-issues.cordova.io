3b01adf679337af316af307a5d79d0f3c3ec6e5c
Brian LeRoux <brian@westcoastlogic.com>
2013-01-31 11:54:34 -0800

    boo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require('express')
+/*var express = require('express')
 ,   app = module.exports = express()
 
 app.get('/', function(req, res) {
@@ -8,5 +8,14 @@ app.get('/', function(req, res) {
 app.get('/:id', function(req, res) {
     res.redirect('https://issues.apache.org/jira/browse/CB-' + req.params.id)
 })
+*/
+
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+      response.send('Hello World!');
+});
 
 app.listen(process.env.PORT || 3000)
